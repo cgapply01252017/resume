e06537e177c208e1e53a2c0e2b3636e80907678b
Chris <chris.geelhoed@gmail.com>
2017-01-24 17:25:29 -0800

    added javascript to make responsive, next time start correctly...

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+console.log("width:", screen.width);
+console.log(document.getElementsByClassName("page"));
+
+let pages = document.getElementsByClassName("page");
+
+for(let i=0; i<pages.length; i++) { pages[i].style.height = "100px"; }
+
+if(screen.width > 1200) {
+    for(let i=0; i<pages.length; i++) { pages[i].style.height = "1000px"; }
+}
+else if(screen.width > 800) {
+    for(let i=0; i<pages.length; i++) { pages[i].style.height = "1200px"; }
+}
+else if(screen.width > 400) {
+    for(let i=0; i<pages.length; i++) { pages[i].style.height = "1300px"; }
+}
+else {
+    for(let i=0; i<pages.length; i++) { pages[i].style.height = "1400px"; }
+}
+
 var circle_od = window.innerWidth > 500 ? 70 : 45;
 //circle_od = 45;
 var stroke_length = Math.ceil(2 * 3.14159 * 0.8 * circle_od);
